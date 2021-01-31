7a1953a2850ac3699bb02c071d5a21a6f912030a
LAShZ <lavch@qq.com>
2021-01-31 13:24:31 +0800

    finish clause parse

---
+++ clause/generator.go
@@ -0,0 +1,78 @@
+package clause
+
+import (
+	"fmt"
+	"strings"
+)
+
+type generator func(values ...interface{}) (string, []interface{})
+
+var generators map[Type]generator
+
+func init() {
+	generators = make(map[Type]generator)
+	generators[INSERT] = _insert
+	generators[VALUES] = _values
+	generators[SELECT] = _select
+	generators[LIMIT] = _limit
+	generators[WHERE] = _where
+	generators[ORDERBY] = _orderBy
+}
+
+func genBindVars(num int) string {
+	var vars []string
+	for i := 0; i < num; i++ {
+		vars = append(vars, "?")
+	}
+	return strings.Join(vars,", ")
+}
+
+// _insert generate clause "INSERT INTO $tableName ($fields)"
+func _insert(values ...interface{}) (string, []interface{}) {
+	tableName := values[0]
+	fields := strings.Join(values[1].([]string), ",")
+	return fmt.Sprintf("INSERT INTO %s (%v)", tableName, fields), []interface{}{}
+}
+
+// _values generate clause "VALUES ($v1), ($v2), ..."
+func _values(values ...interface{}) (string, []interface{}) {
+	var bindStr string
+	var sql strings.Builder
+	var vars []interface{}
+	sql.WriteString("VALUES ")
+	for i, value := range values {
+		v := value.([]interface{})
+		if bindStr == "" {
+			bindStr = genBindVars(len(v))
+		}
+		sql.WriteString(fmt.Sprintf("(%v)", bindStr))
+		if i != len(values)-1 {
+			sql.WriteString(", ")
+		}
+		vars = append(vars, v...)
+	}
+	return sql.String(), vars
+}
+
+// _select generate clause "SELECT $fields FROM $tableName"
+func _select(values ...interface{}) (string, []interface{}) {
+	tableName := values[0]
+	fields := strings.Join(values[1].([]string), ",")
+	return fmt.Sprintf("SELECT %v FROM %s", fields, tableName), []interface{}{}
+}
+
+// _limit generate clause "LIMIT $num"
+func _limit(values ...interface{}) (string, []interface{}) {
+	return "LIMIT ?", values
+}
+
+// _where generate clause "WHERE $desc"
+func _where(values ...interface{}) (string, []interface{}) {
+	desc, vars := values[0], values[1:]
+	return fmt.Sprintf("WHERE %s", desc), vars
+}
+
+// _orderBy generate clause "ORDER BY $fields"
+func _orderBy(values ...interface{}) (string, []interface{}) {
+	return fmt.Sprintf("ORDER BY %s", values[0]), []interface{}{}
+}
